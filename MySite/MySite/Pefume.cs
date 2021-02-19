32f8cb92588f136566c25b65d6b41cc3c8e9615a
CaMoCBaJL <gorka19800@gmail.com>
2021-02-19 23:28:44 +0400

    сайт

---
+++ MySite/MySite/Pefume.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite
+{
+    public class Pefume
+    {
+        public string PerfumeId { get; set; }
+        public double PerfumeCost { get; set; }
+        public string PerfumeProducer { get; set; }
+
+    }
+}
---
+++ MySite/MySite/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite
+{
+    public class User
+    {
+        public User()
+        {
+            UserName = "Nine";
+            Password = "Nine!";
+        }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public void ShowUser()
+        {
+            Console.WriteLine($"UserName : {UserName}");
+            Console.WriteLine($"Password : {Password}");
+        }
+    }
+}
